6fea831fc9509451d4baf46d0a86f99afe26fc74
yukihane <yukihane.feather@gmail.com>
2015-01-08 21:44:41 +0900

    listとsetの例題を追加

--- ex01/src/main/java/com/github/yukihane/hazelcast/SimpleMapExample.java
+++ ex01/src/main/java/com/github/yukihane/hazelcast/SimpleMapExample.java
@@ -2,13 +2,18 @@ package com.github.yukihane.hazelcast;
 
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
 import com.hazelcast.core.IMap;
+import com.hazelcast.core.ISet;
 
 public class SimpleMapExample {
     public static void main(final String[] args) {
+
+        // Sec. Mapping back to the real world
+
         final HazelcastInstance hz = Hazelcast.newHazelcastInstance();
 
-        final IMap<String, String> capitals = hz.<String, String> getMap("capitals");
+        final IMap<String, String> capitals = hz.getMap("capitals");
         capitals.put("GB", "London");
         capitals.put("FR", "Paris");
         capitals.put("US", "Washington DC");
@@ -16,5 +21,19 @@ public class SimpleMapExample {
 
         System.err.println("Known capital cities: " + capitals.size());
         System.err.println("Capital city of GB:" + capitals.get("GB"));
+
+        // Sec. Sets, lists, and queues
+
+        final ISet<String> cities = hz.getSet("cities");
+        cities.addAll(capitals.values());
+        cities.add("London");
+        cities.add("Rome");
+        cities.add("New York");
+
+        final IList<String> countries = hz.getList("countries");
+        countries.addAll(capitals.keySet());
+        countries.add("CA");
+        countries.add("DE");
+        countries.add("GB");
     }
 }
