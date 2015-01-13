3b307d561351017ee76c9c90b2aa23e02175c0a0
yukihane <yukihane.feather@gmail.com>
2015-01-13 19:20:25 +0900

    chapter3 の AtomicMapExample を実装

---
+++ src/main/java/com/github/yukihane/hazelcast/ch03/AtomicMapExample.java
@@ -0,0 +1,26 @@
+package com.github.yukihane.hazelcast.ch03;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+public class AtomicMapExample {
+
+    public static void main(final String[] args) {
+        final HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+
+        final IMap<String, String> capitals = hz.getMap("capitals");
+
+        capitals.putIfAbsent("GB", "Winchester");
+        System.err.println("Capital of GB (until 1066): " + capitals.get("GB"));
+
+        final String actualCapital = capitals.putIfAbsent("GB", "London");
+        System.err.println("Failed to put as alreadty present: " + capitals.get("GB") + "=" + actualCapital);
+
+        final boolean r1 = capitals.replace("GB", "Southampton", "London");
+        System.err.println("Failed to replace as incorrect old value: " + capitals.get("GB") + "; [" + r1 + "]");
+
+        final boolean r2 = capitals.replace("GB", "Winchester", "London");
+        System.err.println("Capital of GB (since 1066): " + capitals.get("GB") + "; [" + r2 + "]");
+    }
+}
